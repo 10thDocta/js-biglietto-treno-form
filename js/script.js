94e6c461087cf3fabe9cbd54784fac56073182e9
Davide Preti <davide.preti86@gmail.com>
2020-08-07 21:19:45 +0200

    Completo al 80% - manca un controllo migliore sul numero km

--- js/script.js
+++ js/script.js
@@ -1,48 +1,71 @@
-// definisco le variabili con le quali poi andrò a manipolare il codice HTML
-const km = document.getElementById('km');
-const age = document.getElementById('age');
-const discount = document.getElementById('discount');
-const price = document.getElementById('price');
+// definisco le variabili per l'HTML
+const tableName = document.getElementById('table_name');
+const tableDiscount = document.getElementById('table_discount');
+const tableCarriage = document.getElementById('table_carriage');
+const tableCP = document.getElementById('table_cp');
+const tablePrice = document.getElementById('table_price');
+const hide = document.getElementById("hide");
 
-// ottengo il dato "chilometri" tramite comando prompt
-const kmInput = parseInt(prompt('Chilometri da percorrere?'));
-km.innerHTML = kmInput;
-// ottengo il dato "age" tramite comando prompt
-const ageInput = parseInt(prompt('Qual è la tua età?'));
-age.innerHTML = ageInput;
+// definisco le variabili dei bottoni
+const calcPriceBtn = document.getElementById("calcPrice_Btn");
+const resetBtn = document.getElementById("reset_Btn");
 
-const kmPrice = 0.21;
-const currentPrice = kmInput * kmPrice;
+// funzione per il reset
+const reset = () => {
+  tableName.innerHTML = "";
+  tableDiscount.innerHTML = "";
+  tableCarriage.innerHTML = "";
+  tableCP.innerHTML = "";
+  tablePrice.innerHTML = "";
+  hide.className = "hide";
+};
+
 
 // funzione per messaggio di errore in caso di dati non nel formato corretto
-const errorMessage = (kmInput, ageInput) => {
-  if (isNaN(kmInput) && isNaN(ageInput)) {
-    km.innerHTML = '>> Correggere questo dato <<';
-    age.innerHTML = '>> Correggere questo dato <<';
-  } else if (isNaN(kmInput)) {
-    km.innerHTML = '>> Correggere questo dato <<';
+const checkInput = (name, km, age) => {
+  if (name.length == 0 || km <= 0) {
+    alert('Valori inseriti non corretti');
+    return false;
+  } else if (age != "maggiorenne" && age != "minorenne" && age != "over65") {
+    alert('Valori inseriti non corretti');
+    return false;
   } else {
-    age.innerHTML = '>> Correggere questo dato <<';
-  }
-
-  price.innerHTML = 'Impossibile da calcolare';
-};
-
-// funzione per il calcolo del prezzo del biglietto
-const calcPrice = (kmInput, ageInput) => {
-  if (isNaN(kmInput) || isNaN(ageInput)) {
-    errorMessage(kmInput, ageInput);
-    alert('Inserire solo dati numerici');
-  } else if (ageInput < 18) {
-    discount.innerHTML = '20%';
-    price.innerHTML = `${(currentPrice - (currentPrice * 20) / 100).toFixed(2)} €`;
-  } else if (ageInput > 65) {
-    discount.innerHTML = '40%';
-    price.innerHTML = `${(currentPrice - (currentPrice * 40) / 100).toFixed(2)} €`;
-  } else {
-    discount.innerHTML = 'Nessuno';
-    price.innerHTML = `${currentPrice.toFixed(2)} €`;
+    return true;
   }
 };
 
-calcPrice(kmInput, ageInput);
+const test = () => {
+  reset();
+  // definisco le variabili che leggeranno i valori immessi
+  let name = document.getElementById("name").value;
+  let km = parseInt(document.getElementById("km").value, 10);
+  let age = document.getElementById("age").value;
+
+  if (checkInput(name, km, age)) {
+    let kmPrice = 0.21;
+    let currentPrice = km * kmPrice;
+
+    let randomCarriage = Math.floor(Math.random() * 7) + 1;
+    let randomCP = Math.floor(Math.random() * 10000) + 90000;
+
+    tableName.innerHTML = name;
+    tableCarriage.innerHTML = randomCarriage;
+    tableCP.innerHTML = randomCP;
+
+    if (age == "minorenne") {
+      tableDiscount.innerHTML = "Sconto Young";
+      tablePrice.innerHTML = `${(currentPrice - (currentPrice * 20) / 100).toFixed(2)} €`;
+    } else if (age == "over65") {
+      tableDiscount.innerHTML = "Sconto Over 65";
+      tablePrice.innerHTML = `${(currentPrice - (currentPrice * 40) / 100).toFixed(2)} €`;
+    } else {
+      tableDiscount.innerHTML = "Nessuna";
+      tablePrice.innerHTML = `${currentPrice.toFixed(2)} €`;
+    }
+  };
+
+  hide.classList.toggle("hide");
+};
+
+calcPriceBtn.addEventListener("click", test);
+resetBtn.addEventListener("click", reset);
