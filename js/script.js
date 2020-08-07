e71e012aec527ed8203b5351f3c9b0eaac3aa755
Davide Preti <davide.preti86@gmail.com>
2020-08-08 00:27:42 +0200

    Migliorato controllo dato "km" e  aggiunto messaggio di errore

--- js/script.js
+++ js/script.js
@@ -5,26 +5,42 @@ const tableCarriage = document.getElementById('table_carriage');
 const tableCP = document.getElementById('table_cp');
 const tablePrice = document.getElementById('table_price');
 const hide = document.getElementById("hide");
+const hideTest = document.getElementById("hide_message_error");
 
 // definisco le variabili dei bottoni
 const calcPriceBtn = document.getElementById("calcPrice_Btn");
 const resetBtn = document.getElementById("reset_Btn");
 
 // funzione per il reset
-const reset = () => {
-  tableName.innerHTML = "";
-  tableDiscount.innerHTML = "";
-  tableCarriage.innerHTML = "";
-  tableCP.innerHTML = "";
-  tablePrice.innerHTML = "";
-  hide.className = "hide";
+const reset = (numb = 0) => {
+  // num = 1 è arriva solo da calcPrice
+  if (numb == 1) {
+    tableName.innerHTML = "";
+    tableDiscount.innerHTML = "";
+    tableCarriage.innerHTML = "";
+    tableCP.innerHTML = "";
+    tablePrice.innerHTML = "";
+    hide.className = "hide";
+    hideTest.className = "hide";
+  } else {
+    tableName.innerHTML = "";
+    tableDiscount.innerHTML = "";
+    tableCarriage.innerHTML = "";
+    tableCP.innerHTML = "";
+    tablePrice.innerHTML = "";
+    hide.className = "hide";
+    hideTest.className = "hide";
+    document.getElementById("name").value = "";
+    document.getElementById("km").value = "";
+  }
 };
 
 
 // funzione per messaggio di errore in caso di dati non nel formato corretto
 const checkInput = (name, km, age) => {
-  if (name.length == 0 || km <= 0) {
-    alert('Valori inseriti non corretti');
+  if (name.length == 0 || km <= 0 || isNaN(km)) {
+    // alert('Valori inseriti non corretti');
+    hideTest.classList.toggle("hide");
     return false;
   } else if (age != "maggiorenne" && age != "minorenne" && age != "over65") {
     alert('Valori inseriti non corretti');
@@ -34,13 +50,18 @@ const checkInput = (name, km, age) => {
   }
 };
 
-const test = () => {
-  reset();
-  // definisco le variabili che leggeranno i valori immessi
+// funzione per calcolare il prezzo del biglietto
+const calcPrice = () => {
+
+  // faccio un reset appena viene invocata la funzione
+  reset(1);
+
+  // definisco le variabili che leggeranno i valori immessi, ma solo dopo il click sul bottone
   let name = document.getElementById("name").value;
   let km = parseInt(document.getElementById("km").value, 10);
   let age = document.getElementById("age").value;
 
+  // ciclo if etnra se la funzione di controllo dei dati immessi restituisce vero
   if (checkInput(name, km, age)) {
     let kmPrice = 0.21;
     let currentPrice = km * kmPrice;
@@ -62,10 +83,10 @@ const test = () => {
       tableDiscount.innerHTML = "Nessuna";
       tablePrice.innerHTML = `${currentPrice.toFixed(2)} €`;
     }
-  };
 
-  hide.classList.toggle("hide");
+    hide.classList.toggle("hide");
+  };
 };
 
-calcPriceBtn.addEventListener("click", test);
+calcPriceBtn.addEventListener("click", calcPrice);
 resetBtn.addEventListener("click", reset);
